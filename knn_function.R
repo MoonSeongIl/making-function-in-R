ddf4986d060fb314ffc5a7026466042e30ea73f7
MoonSeongIl <moonsil802@gmail.com>
2021-09-13 23:42:29 +0900

    revise the knn_function in r

--- knn_function.R
+++ knn_function.R
@@ -1,83 +1,84 @@
 rm(list=ls())
+library(dplyr)
+set.seed(1234)
+
 # knn Algorithms
 
 # 1. Y = X+e
-x1 <- rnorm(10000,m=0,sd=1)
-e <- rnorm(10000,m=0,sd=1)
+x1 <- rnorm(100,m=0,sd=1)
+head(x1)
+e <- rnorm(100,m=0,sd=1)
 y<- x1+e
+head(y)
+x1[order(x1)]
 
+# 2. Y = X(1-X)+e
+x2 <- rnorm(100,m=0,sd=1)
+e <- rnorm(100,m=0,sd=1)
+y2<- x2*(1-x2)+e
 
-# Euclidean distance
+# knn function
 knn_function <- function(x,y,k){
+  
+  #normalization function
   normalize <- function(x){
-    num <- x-min(x)
-    denom <- max(x) - min(x)
-    return (num/denom)
-  }
-  eucliden_distance<- function(x1,x2){
-    sqrt(sum((x1-x2)^2))
+    num <- (x-mean(x)) / sd(x)
+    return(num)
   }
   
-  # normalization
-  x<-normalize(x)
-  y<-normalize(y)
-  
-  # x,y 값 순서 order
-  x<-x[(order(x))]
-  y<-y[(order(y))]
-  
-  #length 설정
-  num = length(x)
-  
-  #matrix 만들기
-  data = matrix(y,x,nrow=num,ncol=2)
-  
-  #k 설정
-  k=k
-  
-  #data_frame 만들기.
-  data = as.data.frame(data)
-  
-  distance_result = matrix(rep(0,num),nrow=num,ncol=1)
-  
-  for(i in 1:num){
-    distance_result[i] <- eucliden_distance(data[[2]][i],data[[2]][1])
+  # eucliden_distance function
+  eucliden_distance<- function(x,y){
+    sqrt(sum((x-y)^2))
   }
   
-  data<-cbind(data,distance_result)
-  colnames(data)<-c('y','x','distance_result')
-  knn_fitted_value = mean(data[["y"]][1:k+1])
-  print(knn_fitted_value)
-}
+  # distance matrix function
+  distance_matrix<- function(x){
+    if(is.matrix(x)==F){
+      n<-length(x)
+    }else {
+      n<-dim(x)[1]
+    }
+    distance_result_matrix = matrix(rep(0,n^2),n,n)
+    if(is.matrix(x)==F){
+      for(i in 1:n){
+        for(j in 1:n){
+          distance_result_matrix[j,i] = eucliden_distance(x[j],x[i])
+          }
+        }
+      }else {
+          for(i in 1:n){
+            for(j in 1:n){
+              distance_result_matrix[j,i] = eucliden_distance(x[j,],x[i,])
+            }
+          }
+        }
+    distance_result_matrix = distance_result_matrix+t(distance_result_matrix)
+    return(distance_result_matrix)
+  }
+  
+  if(is.matrix(x)==F){
+    x_bar = mean(x)
+    x_sd = sd(x)
+    x_normal = (x-x_bar)/x_sd
+  }else {
+    x_bar <- apply(x,2,mean)
+    x_sd <- apply(x,2,sd)
+    x_normal <- matrix(rep(0,dim(x)[1]*dim(x)[2]),nrow = dim(x)[1],ncol = dim(x)[2])
+    
+      for(i in 1:dim(x)[2]){
+        x_normal[,i]<-(x[,i] - x_bar[i])/x_sd[i]
+      }
+  }
+  dist_matrix <-distance_matrix(x_normal)
+  
+  matrix_order <- apply(dist_matrix,1,order)
+  
+  fitted_value <- c()
+  for(q in 1:dim(matrix_order)[1]){
+    fitted_value[q] <- mean(y[matrix_order[2:(k+1),q]])
+    }
+    return(fitted_value)
+  }
+knn_function(x1,y,k=5)
+knn_function(x2,y2,k=5)
 
-knn_function(x1,y,k=10)
-
-
-# # x1, y 값 순서 order 
-# x1<-x1[(order(x1))]
-# y<-y[(order(y))]
-# 
-# # matrix 만들기.
-# data = matrix(y,x1,nrow=10000,ncol=2)
-# 
-# # columns 이름 변경
-# colnames(data)<-c('y','x')
-# # k값 설정
-# k=3
-# # length 설정
-# num = dim(data)[1]
-# 
-# data<-as.data.frame(data)
-# 
-# # distance를 했을 때 가장 가까운 x값을 알 수 있음.
-# distance_result<-matrix(rep(0,num),nrow=num,ncol=1)
-# 
-# for(i in 1:num){
-#   distance_result[i] <- eucliden_distance(data[[2]][i],data[[2]][1])
-# }
-# data<-cbind(data,distance_result)
-# colnames(data)<-c('y','x','distance_result')
-# head(data)
-# data[["y"]][1:k+1]
-# knn_fitted_value = mean(data[["y"]][1:11])
-# print(knn_fitted_value)
